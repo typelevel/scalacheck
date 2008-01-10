d92a023be45fb377590d48a0c085d38bcc124dfa
Rickard Nilsson <rickynils@gmail.com>
2008-01-10 10:00:36 +0000

    Added ScalaCheck info at top of each source file.

--- src/org/scalacheck/Util.scala
+++ src/org/scalacheck/Util.scala
@@ -1,3 +1,12 @@
+/*-------------------------------------------------------------------------*\
+**  ScalaCheck                                                             **
+**  Copyright (c) 2007-2008 Rickard Nilsson. All rights reserved.          **
+**  http://code.google.com/p/scalacheck/                                   **
+**                                                                         **
+**  This software is released under the terms of the Revised BSD License.  **
+**  There is NO WARRANTY. See the file LICENSE for the full text.          **
+\*-------------------------------------------------------------------------*/
+
 package org.scalacheck
 
 object Util {
