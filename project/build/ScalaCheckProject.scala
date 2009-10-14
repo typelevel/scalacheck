c84701cf8f99fe6ff6e117587fb231497d3a3f35
Rickard Nilsson <rickynils@gmail.com>
2009-10-14 20:38:11 +0000

    Changed SBT project to build agains Scala 2.8 nightlies

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -15,4 +15,11 @@ class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
   val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
   val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadoc"), Nil, None)
   override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
+
+  //def specificSnapshotRepo = Resolver.url("scala-nightly")
+  //artifacts("http://scala-tools.org/repo-snapshots/[organization]/[module]/2.8.0-SNAPSHOT/[artifact]-[revision].[ext]")
+  //mavenStyle()
+  //val nightlyScala = ModuleConfiguration("org.scala-lang", "*", "2.8.0-.*", specificSnapshotRepo)
+
+  override def deliverScalaDependencies = Nil
 }
