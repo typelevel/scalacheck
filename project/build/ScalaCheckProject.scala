74a2c45b42c5e414b109afc3ca9da13809f4320b
Rickard Nilsson <rickynils@gmail.com>
2009-10-19 11:09:17 +0000

    Cleaned up project file.

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -2,24 +2,22 @@ import sbt._
 
 class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  override def crossScalaVersions = Set("2.7.2", "2.7.3", "2.7.4", "2.7.5", "2.7.6")
+  //override def crossScalaVersions = Set("2.7.2", "2.7.3", "2.7.4", "2.7.5", "2.7.6")
 
   override def managedStyle = ManagedStyle.Maven
 
+  override def packageDocsJar = defaultJarPath("-javadoc.jar")
+  override def packageSrcJar= defaultJarPath("-sources.jar")
+
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
+
+  override def deliverScalaDependencies = Nil
+
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
+
+  val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
+  val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadoc"), Nil, None)
 
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
 
-  override def packageDocsJar = defaultJarPath("-javadoc.jar")
-  override def packageSrcJar= defaultJarPath("-sources.jar")
-  val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
-  val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadoc"), Nil, None)
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
-
-  //def specificSnapshotRepo = Resolver.url("scala-nightly")
-  //artifacts("http://scala-tools.org/repo-snapshots/[organization]/[module]/2.8.0-SNAPSHOT/[artifact]-[revision].[ext]")
-  //mavenStyle()
-  //val nightlyScala = ModuleConfiguration("org.scala-lang", "*", "2.8.0-.*", specificSnapshotRepo)
-
-  override def deliverScalaDependencies = Nil
 }
