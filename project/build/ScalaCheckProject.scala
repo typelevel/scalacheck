52cf76aeba2987aeb13556420c2de90cae423ec8
Rickard Nilsson <rickynils@gmail.com>
2009-11-30 19:43:25 +0000

    Temporaily disabled scaladoc generation since it causes an NPE in Scala

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -2,14 +2,14 @@ import sbt._
 
 class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  override def crossScalaVersions = List("2.8.0.Beta1-RC2")
+  override def crossScalaVersions = List("2.8.0.Beta1-RC1", "2.8.0.Beta1-RC2")
 
   override def managedStyle = ManagedStyle.Maven
 
   override def packageDocsJar = defaultJarPath("-javadoc.jar")
   override def packageSrcJar= defaultJarPath("-sources.jar")
 
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(/*packageDocs,*/ packageSrc)
 
   override def deliverScalaDependencies = Nil
 
