4fe2b79a1393f87768ec6353e4255cd5382a1979
Rickard Nilsson <rickynils@gmail.com>
2009-12-21 22:17:19 +0000

    Updated to Scala 2.8.0 Beta1 RC5

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -2,16 +2,18 @@ import sbt._
 
 class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  override def crossScalaVersions = List("2.8.0.Beta1-RC3", "2.8.0.Beta1-RC2")
+  override def crossScalaVersions = List("2.8.0.Beta1-RC5", "2.8.0.Beta1-RC4")
 
   override def managedStyle = ManagedStyle.Maven
 
   override def packageDocsJar = defaultJarPath("-javadoc.jar")
   override def packageSrcJar= defaultJarPath("-sources.jar")
 
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(/*packageDocs,*/ packageSrc)
+  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
 
   override def deliverScalaDependencies = Nil
+
+  override def documentOptions = Nil
 
   val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
 
