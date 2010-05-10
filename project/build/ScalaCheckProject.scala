fa37f65c732842419433cfe9b02e971c4baafb40
Rickard Nilsson <rickynils@gmail.com>
2010-05-10 17:18:48 +0000

    Updated to Scala 2.8.0-RC2

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  override def crossScalaVersions = List("2.8.0.RC1")
+  override def crossScalaVersions = List("2.8.0.RC1", "2.8.0.RC2")
 
   override def managedStyle = ManagedStyle.Maven
 
