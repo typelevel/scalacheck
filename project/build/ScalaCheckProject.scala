a72d9f4dfad55babc362267bcfb1304bfd801bb5
Rickard Nilsson <rickynils@gmail.com>
2010-06-15 06:21:13 +0000

    Updated to SBT 0.7.4 and Scala 2.8.0.RC4

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  override def crossScalaVersions = List("2.8.0.RC1", "2.8.0.RC2", "2.8.0.RC3")
+  override def crossScalaVersions = List("2.8.0.RC1", "2.8.0.RC2", "2.8.0.RC3", "2.8.0.RC4")
 
   override def managedStyle = ManagedStyle.Maven
 
--- sbt
+++ sbt
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Xmx1024M -jar `dirname $0`/tools/sbt-launch-0.7.3.jar "$@"
+java -Xmx1024M -jar `dirname $0`/tools/sbt-launch-0.7.4.jar "$@"
