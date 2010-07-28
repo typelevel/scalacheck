ffdf9230976e0fc32944952ab9ff82c69bd35da8
Rickard Nilsson <rickynils@gmail.com>
2010-07-28 10:54:13 +0000

    Fixed issue #69

--- src/main/scala/org/scalacheck/Arbitrary.scala
+++ src/main/scala/org/scalacheck/Arbitrary.scala
@@ -92,7 +92,7 @@ object Arbitrary {
 
   /** Arbitrary instance of Long */
   implicit lazy val arbLong: Arbitrary[Long] = Arbitrary(
-    Gen.chooseNum(Long.MinValue >> 2, Long.MaxValue >> 2)
+    Gen.chooseNum(Long.MinValue / 2, Long.MaxValue / 2)
   )
 
   /** Arbitrary instance of Float */
@@ -107,7 +107,7 @@ object Arbitrary {
   /** Arbitrary instance of Double */
   implicit lazy val arbDouble: Arbitrary[Double] = Arbitrary(
     Gen.chooseNum(
-      Double.MinValue, Double.MaxValue
+      Double.MinValue / 2, Double.MaxValue / 2
       // As above.  Perhaps behind some option?
       // Double.Epsilon, Double.NaN, Double.PositiveInfinity, Double.NegativeInfinity
     )
