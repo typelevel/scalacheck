45f2749d5218f2ae4d958d34c65f1a485aa719a2
Rickard Nilsson <rickynils@gmail.com>
2011-02-15 15:19:14 +0000

    Cleaned up build files. Incremented version to 1.9-SNAPSHOT. Building against Scala 2.9.0-SNAPSHOT.

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -2,21 +2,21 @@ import sbt._
 
 class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  override def crossScalaVersions = List("2.8.1")
-
   override def managedStyle = ManagedStyle.Maven
-
   override def packageDocsJar = defaultJarPath("-javadoc.jar")
   override def packageSrcJar= defaultJarPath("-sources.jar")
-
-  override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
-
+  override def packageToPublishActions = 
+    super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
   override def deliverScalaDependencies = Nil
-
   override def documentOptions = Nil
 
-  //val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/snapshots/"
-  val publishTo = "Scala Tools Nexus" at "http://nexus.scala-tools.org/content/repositories/releases/"
+  val scalaToolsSnapshots = "Scala Tools Nexus" at 
+    "http://nexus.scala-tools.org/content/repositories/snapshots/"
+
+  val scalaToolsReleases = "Scala Tools Nexus" at 
+    "http://nexus.scala-tools.org/content/repositories/releases/"
+
+  val publishTo = scalaToolsSnapshots
 
   val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
   val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadoc"), Nil, None)
@@ -24,5 +24,4 @@ class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
   val depTestInterface = "org.scala-tools.testing" %  "test-interface" % "0.5"
 
   Credentials(Path.userHome / ".ivy2" / ".credentials", log)
-
 }
