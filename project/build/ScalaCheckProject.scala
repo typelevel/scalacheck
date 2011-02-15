639552c0ed0a92ee3d1dd1264bcd7916ac090c2a
Rickard Nilsson <rickynils@gmail.com>
2011-02-15 15:48:59 +0000

    Updated to SBT 0.7.5.RC0. Fixed bug in repository definitions.

--- project/build/ScalaCheckProject.scala
+++ project/build/ScalaCheckProject.scala
@@ -5,16 +5,16 @@ class ScalaCheckProject(info: ProjectInfo) extends DefaultProject(info) {
   override def managedStyle = ManagedStyle.Maven
   override def packageDocsJar = defaultJarPath("-javadoc.jar")
   override def packageSrcJar= defaultJarPath("-sources.jar")
-  override def packageToPublishActions = 
+  override def packageToPublishActions =
     super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
   override def deliverScalaDependencies = Nil
   override def documentOptions = Nil
 
-  val scalaToolsSnapshots = "Scala Tools Nexus" at 
-    "http://nexus.scala-tools.org/content/repositories/snapshots/"
-
-  val scalaToolsReleases = "Scala Tools Nexus" at 
+  val scalaToolsReleases = "Scala Tools Releases" at
     "http://nexus.scala-tools.org/content/repositories/releases/"
+
+  val scalaToolsSnapshots = "Scala Tools Snapshots" at
+    "http://nexus.scala-tools.org/content/repositories/snapshots/"
 
   val publishTo = scalaToolsSnapshots
 
