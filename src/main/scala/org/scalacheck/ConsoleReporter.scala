12cf2521052a2daa85d58a66a2c702ea46a02a4d
Paul Phillips <paulp@improving.org>
2011-05-02 20:24:31 +0000

    Eliminated some deprecated warnings in @deprecated annotations by updating with the version.

--- src/main/scala/org/scalacheck/ConsoleReporter.scala
+++ src/main/scala/org/scalacheck/ConsoleReporter.scala
@@ -37,21 +37,21 @@ object ConsoleReporter {
    *  the given verbosity */
   def apply(verbosity: Int = 0) = new ConsoleReporter(verbosity)
 
-  @deprecated("(v1.8)")
+  @deprecated("(v1.8)", "1.8")
   def propReport(s: Int, d: Int) = {
     if(d == 0) printf("\rPassed %s tests\r", s)
     else printf("\rPassed %s tests; %s discarded\r", s, d)
     Console.flush
   }
 
-  @deprecated("(v1.8)")
+  @deprecated("(v1.8)", "1.8")
   def propReport(pName: String, s: Int, d: Int) = {
     if(d == 0) printf("\r  %s: Passed %s tests\r", pName, s)
     else printf("\r  %s: Passed %s tests; %s discarded\r", pName, s, d)
     Console.flush
   }
 
-  @deprecated("(v1.8)")
+  @deprecated("(v1.8)", "1.8")
   def testReport(res: Test.Result) = {
     print(List.fill(78)(' ').mkString)
     val s = (if(res.passed) "+ " else "! ") + pretty(res, Params(0))
@@ -59,7 +59,7 @@ object ConsoleReporter {
     res
   }
 
-  @deprecated("(v1.8)")
+  @deprecated("(v1.8)", "1.8")
   def testStatsEx(res: Test.Result): Unit = testStatsEx("", res)
 
   def testStatsEx(msg: String, res: Test.Result) = {
