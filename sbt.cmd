40f0ed9d9aead58fb25470e92c13f6a0c2656dd0
Tony Morris <tmorris@tmorris.net>
2011-05-28 11:12:51 +1000

    Add SBT windows launcher

---
+++ sbt.cmd
@@ -0,0 +1,4 @@
+@echo off
+set SCRIPT_DIR=%~dp0
+if defined PROXY_HOST set PROXY_OPTS=-Dhttp.proxyHost=%PROXY_HOST% -Dhttp.proxyPort=%PROXY_PORT% -Dhttp.proxyUser=%PROXY_USER% -Dhttp.proxyPassword=%PROXY_PASSWORD%
+java %SBT_OPTS% %PROXY_OPTS% -Xmx1024M -jar "%SCRIPT_DIR%sbt-launch-0.7.4.jar" %*
