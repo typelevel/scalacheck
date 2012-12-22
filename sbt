c88138c6ba78b4e3933fd9c8a4d792b053205124
Rickard Nilsson <rickynils@gmail.com>
2012-12-22 01:06:03 +0100

    Update sbt to 0.12.1

--- sbt
+++ sbt
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java $SBT_OPTS -Xmx1024M -XX:MaxPermSize=512m -jar `dirname $0`/tools/sbt-launch-0.11.3.jar "$@"
+java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=384M -jar `dirname $0`/tools/sbt-launch-0.12.1.jar "$@"
--- sbt.cmd
+++ sbt.cmd
@@ -1,4 +1,4 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 if defined PROXY_HOST set PROXY_OPTS=-Dhttp.proxyHost=%PROXY_HOST% -Dhttp.proxyPort=%PROXY_PORT% -Dhttp.proxyUser=%PROXY_USER% -Dhttp.proxyPassword=%PROXY_PASSWORD%
-java %SBT_OPTS% %PROXY_OPTS% -Xmx1024M -jar "%SCRIPT_DIR%/tools/sbt-launch-0.11.3.jar" %*
+java %SBT_OPTS% %PROXY_OPTS% -Xmx1024M -jar "%SCRIPT_DIR%/tools/sbt-launch-0.12.1.jar" %*
