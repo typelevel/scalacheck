a3612653d246bd3f8dde52aa963060fc829b50de
Rickard Nilsson <rickynils@gmail.com>
2013-05-23 09:41:13 +0200

    Add minimal example sbt project

---
+++ examples/simple-sbt/build.sbt
@@ -0,0 +1,6 @@
+name := "scalacheck-demo"
+
+scalaVersion := "2.10.1"
+
+libraryDependencies += 
+  "org.scalacheck" %% "scalacheck" % "1.10.1" % "test"
---
+++ examples/simple-sbt/src/test/scala/Demo.scala
@@ -0,0 +1,10 @@
+import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+
+object ScalaCheckDemo extends Properties("Demo") {
+
+  property("myprop") = forAll { l: List[Int] =>
+    l.reverse.reverse == l
+  }
+
+}
