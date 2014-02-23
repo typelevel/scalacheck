6a997275c4a7c9c891648ac31f61bb36a1c3077c
Rickard Nilsson <rickynils@gmail.com>
2014-02-23 22:29:09 +0100

    sbt 0.13.1

--- sbt.cmd
+++ sbt.cmd
@@ -1,4 +1,4 @@
 @echo off
 set SCRIPT_DIR=%~dp0
 if defined PROXY_HOST set PROXY_OPTS=-Dhttp.proxyHost=%PROXY_HOST% -Dhttp.proxyPort=%PROXY_PORT% -Dhttp.proxyUser=%PROXY_USER% -Dhttp.proxyPassword=%PROXY_PASSWORD%
-java %SBT_OPTS% %PROXY_OPTS% -Xmx1024M -jar "%SCRIPT_DIR%/tools/sbt-launch-0.13.0.jar" %*
+java %SBT_OPTS% %PROXY_OPTS% -Xmx1024M -jar "%SCRIPT_DIR%/tools/sbt-launch-0.13.1.jar" %*
