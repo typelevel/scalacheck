4db2beeed3b4d76071f1db499d2b80bb2e48dc19
Rickard Nilsson <rickynils@gmail.com>
2015-06-15 15:35:57 +0200

    Refactor Travis script a bit

--- tools/travis-script.sh
+++ tools/travis-script.sh
@@ -4,14 +4,15 @@ set -evu
 sbt_cmd=(sbt ++$TRAVIS_SCALA_VERSION)
 
 if [[ "$PLATFORM" == "js" ]]; then
+  TESTS=200
   sbt_cmd+=("set scalaJSStage in Global := FastOptStage")
+else
+  TESTS=5000
 fi
 
-if [[ "$SBT_PARALLEL" == "no" ]]; then
-  sbt_cmd+=("set parallelExecution in ThisBuild := false")
-fi
+sbt_cmd+=("set parallelExecution in ThisBuild := $SBT_PARALLEL")
 
-for t in clean update compile "testOnly * -- -s $TESTS -w $WORKERS" mimaReportBinaryIssues doc; do
+for t in clean compile "testOnly * -- -s $TESTS -w $WORKERS" mimaReportBinaryIssues doc; do
   sbt_cmd+=("$PLATFORM/$t")
 done
 
