0671ce9b149c60df1d765aef4ec8721993cb0deb
xuwei-k <6b656e6a69@gmail.com>
2016-11-26 16:42:17 +0900

    add Cogen[Symbol]

--- src/main/scala/org/scalacheck/Cogen.scala
+++ src/main/scala/org/scalacheck/Cogen.scala
@@ -116,6 +116,9 @@ object Cogen extends CogenArities with CogenLowPriority {
   implicit def cogenString: Cogen[String] =
     Cogen.it(_.iterator)
 
+  implicit def cogenSymbol: Cogen[Symbol] =
+    Cogen[String].contramap(_.name)
+
   implicit def cogenList[A: Cogen]: Cogen[List[A]] =
     Cogen.it(_.iterator)
 
