a68e80e7ea375a60fda51a349944555a50a43cad
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2018-05-03 09:14:43 -0400

    Fix 2.12+ MiMa errors
    - method oneOf(Seq)Gen in object Gen's type is different in current
      version, where it is (immutable.Seq)Gen instead of (Seq)Gen
    - method sequence(Traversable,util.Buildable)Gen in object Gen's type
      is different in current version, where it is
      (Iterable,util.Buildable)Gen instead of
      (Traversable,util.Buildable)Gen
    - method properties()Seq in class Properties has a different result
      type in current version, where it is immutable.Seq rather than Seq
    - method checkProperties(Test#Parameters,Properties)Seq in object Test
      has a different result type in current version, where it is
      immutable.Seq rather than Seq
    - method
      canCreateNewSut(java.lang.Object,Traversable,Traversable)Boolean in
      interface commands.Commands's type is different in current version,
      where it is (java.lang.Object,Iterable,Iterable)Boolean instead of
      (java.lang.Object,Traversable,Traversable)Boolean
    - abstract method
      canCreateNewSut(java.lang.Object,Iterable,Iterable)Boolean in
      interface commands.Commands is present only in current version
    - method fromIterable(Traversable)java.lang.Object in interface
      util.Buildable's type is different in current version, where it is
      (Iterable)java.lang.Object instead of (Traversable)java.lang.Object

--- project/MimaSettings.scala
+++ project/MimaSettings.scala
@@ -12,10 +12,13 @@ object MimaSettings {
       removedPrivateMethods.map(exclude[DirectMissingMethodProblem](_)) ++
       newMethods.map(exclude[ReversedMissingMethodProblem](_)) ++
       removedPrivateClasses.map(exclude[MissingClassProblem](_)) ++
+      methodTypeChanges.map(exclude[IncompatibleMethTypeProblem](_)) ++
+      returnTypeChanges.map(exclude[IncompatibleResultTypeProblem](_)) ++
       otherProblems
   )
 
   private def newMethods = Seq(
+    "org.scalacheck.commands.Commands.canCreateNewSut"
   )
 
   private def removedPrivateMethods = Seq(
@@ -23,6 +26,18 @@ object MimaSettings {
 
   private def removedPrivateClasses = Seq(
     "org.scalacheck.Platform$EnableReflectiveInstantiation"
+  )
+
+  private def methodTypeChanges = Seq(
+    "org.scalacheck.Gen.oneOf",
+    "org.scalacheck.Gen.sequence",
+    "org.scalacheck.commands.Commands.canCreateNewSut",
+    "org.scalacheck.util.Buildable.fromIterable"
+  )
+
+  private def returnTypeChanges = Seq(
+    "org.scalacheck.Properties.properties",
+    "org.scalacheck.Test.checkProperties"
   )
 
   private def otherProblems = Seq(
