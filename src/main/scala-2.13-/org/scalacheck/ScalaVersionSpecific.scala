82ca95d271ea4cc2dcd0a536c694757c188ce9b8
Lukas Rytz <lukas.rytz@gmail.com>
2018-05-23 16:49:16 +0200

    Remove Factory type alias, simplify version-dependent Buildable

--- src/main/scala-2.13-/org/scalacheck/ScalaVersionSpecific.scala
+++ src/main/scala-2.13-/org/scalacheck/ScalaVersionSpecific.scala
@@ -1,38 +1,20 @@
+/*-------------------------------------------------------------------------*\
+**  ScalaCheck                                                             **
+**  Copyright (c) 2007-2018 Rickard Nilsson. All rights reserved.          **
+**  http://www.scalacheck.org                                              **
+**                                                                         **
+**  This software is released under the terms of the Revised BSD License.  **
+**  There is NO WARRANTY. See the file LICENSE for the full text.          **
+\*------------------------------------------------------------------------ */
+
 package org.scalacheck
 
-import java.util.ArrayList
-
-import scala.collection.generic.{CanBuildFrom, Sorted}
+import scala.collection.generic.Sorted
 import scala.collection.immutable.Stream
-import scala.collection.mutable.Builder
-import scala.collection.{BitSet, TraversableOnce}
+import scala.collection.TraversableOnce
 
 private[scalacheck] object ScalaVersionSpecific {
   def toLazyList[T](i: TraversableOnce[T]) = i.toStream
-
-  type Factory[-A, +C] = CanBuildFrom[Nothing, A, C]
-
-  def listFactory[T]: CanBuildFrom[List[T], T, List[T]] =
-    new CanBuildFrom[List[T], T, List[T]] with Serializable {
-      def apply(from: List[T]) = List.newBuilder[T]
-      def apply() = List.newBuilder[T]
-    }
-
-  def bitsetFactory[T]: CanBuildFrom[BitSet, Int, BitSet] =
-    new CanBuildFrom[BitSet, Int, BitSet] with Serializable {
-      def apply(from: BitSet) = BitSet.newBuilder
-      def apply() = BitSet.newBuilder
-    }
-
-  def mapFactory[T, U]: CanBuildFrom[Map[T, U], (T, U), Map[T, U]] =
-    new CanBuildFrom[Map[T, U], (T, U), Map[T, U]] with Serializable {
-      def apply(from: Map[T, U]) = Map.newBuilder[T, U]
-      def apply() = Map.newBuilder[T, U]
-    }
-
-  implicit class CBFExt[-A, +C](val cbf: CanBuildFrom[Nothing, A, C]) extends AnyVal {
-    def newBuilder: Builder[A, C] = cbf()
-  }
 
   type LazyList[+A] = Stream[A]
   val LazyList      = Stream
@@ -54,14 +36,4 @@ private[scalacheck] trait CogenVersionSpecific
 // Used in tests
 private[scalacheck] trait GenSpecificationVersionSpecific {
   def infiniteLazyList[T](g: => Gen[T]): Gen[Stream[T]] = Gen.infiniteStream(g)
-}
-
-private[scalacheck] class ArrayListBuilder[T] extends Builder[T, ArrayList[T]] {
-  private val al = new ArrayList[T]
-  def +=(x: T): this.type = {
-    al.add(x)
-    this
-  }
-  def clear(): Unit = al.clear()
-  def result(): ArrayList[T] = al
 }
