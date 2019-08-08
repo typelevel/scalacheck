4735d09f53d9c8a65f2ff124bc2bbe658461d002
Guillaume Martres <smarter@ubuntu.com>
2019-08-08 02:13:39 +0200

    Add scalafix rule for BooleanOperators deprecation
    To run it:
      scalafix --rules=github:smarter/scalacheck/v1_14_1?sha=deprecate-BooleanOperators
    See https://scalacenter.github.io/scalafix/docs/developers/tutorial.html#run-the-rule-from-source-code
    for more information.

--- scalafix/rules/src/main/scala/fix/v1_14_1.scala
+++ scalafix/rules/src/main/scala/fix/v1_14_1.scala
@@ -4,12 +4,28 @@ import scalafix.v1._
 import scala.meta._
 
 class v1_14_1 extends SemanticRule("v1_14_1") {
-
   override def fix(implicit doc: SemanticDocument): Patch = {
-    println("Tree.syntax: " + doc.tree.syntax)
-    println("Tree.structure: " + doc.tree.structure)
-    println("Tree.structureLabeled: " + doc.tree.structureLabeled)
-    Patch.empty
+    BooleanOperatorsRules(doc.tree).asPatch
   }
+}
 
+/** Replace deprecated BooleanOperators by propBoolean */
+object BooleanOperatorsRules {
+  val propObject    = SymbolMatcher.exact("org/scalacheck/Prop.")
+  val deprecatedDef = SymbolMatcher.exact("org/scalacheck/Prop.BooleanOperators().")
+
+  def apply(t: Tree)(implicit doc: SemanticDocument): List[Patch] = t.collect {
+    case Term.Apply(fun, _) if deprecatedDef.matches(fun.symbol) =>
+      fun.collect {
+        case name @ Term.Name("BooleanOperators") =>
+          Patch.replaceTree(name, "propBoolean")
+      }.asPatch
+    // Imports don't have symbols, but import prefixes do, so we match on the
+    // enclosing object.
+    case Importer(ref, importees) if propObject.matches(ref.symbol) =>
+      importees.collect {
+        case Importee.Name(name @ Name("BooleanOperators")) =>
+          Patch.replaceTree(name, "propBoolean")
+      }.asPatch
+  }
 }
