2e0afd9ee06ac175b665a7bdf22764f02e5dc67f
Aaron S. Hawley <aaron.s.hawley@gmail.com>
2019-09-27 07:28:59 -0400

    Add margin to string literals of codegen

--- project/codegen.scala
+++ project/codegen.scala
@@ -42,43 +42,43 @@ def fntype(i: Int) =
   s"(${types(i)}) => Z"
 
 def arbfn(i: Int) = s"""
-  /** Arbitrary instance of Function${i} */
-  implicit def arbFunction${i}[${types(i)},Z](implicit g: Arbitrary[Z], ${coImplicits(i)}): Arbitrary[${fntype(i)}] =
-    Arbitrary(Gen.function${i}(g.arbitrary))
-"""
+  |  /** Arbitrary instance of Function${i} */
+  |  implicit def arbFunction${i}[${types(i)},Z](implicit g: Arbitrary[Z], ${coImplicits(i)}): Arbitrary[${fntype(i)}] =
+  |    Arbitrary(Gen.function${i}(g.arbitrary))
+  |""".stripMargin
 
 def genfn(i: Int) = s"""
-  /** Gen creator for Function${i} */
-  def function${i}[${types(i)},Z](g: Gen[Z])(implicit ${coImplicits(i)}): Gen[${fntype(i)}] =
-    Gen.gen { (p, seed0) =>
-      val f: ${fntype(i)} =
-        (${fnArgs(i)}) => g.pureApply(p, ${nestedPerturbs(i)})
-      Gen.r[${fntype(i)}](
-        r = Some(f),
-        sd = seed0.next
-      )
-    }
-"""
+  |  /** Gen creator for Function${i} */
+  |  def function${i}[${types(i)},Z](g: Gen[Z])(implicit ${coImplicits(i)}): Gen[${fntype(i)}] =
+  |    Gen.gen { (p, seed0) =>
+  |      val f: ${fntype(i)} =
+  |        (${fnArgs(i)}) => g.pureApply(p, ${nestedPerturbs(i)})
+  |      Gen.r[${fntype(i)}](
+  |        r = Some(f),
+  |        sd = seed0.next
+  |      )
+  |    }
+  |""".stripMargin
 
 def tuple(i: Int) = {
   val gens = idents("a",i).map(_+".arbitrary") mkString ","
 
   if (i == 1) {
     s"""
-  /** Arbitrary instance of ${i}-Tuple */
-  implicit def arbTuple$i[${types(i)}](implicit
-    ${wrappedArgs("Arbitrary",i)}
-  ): Arbitrary[Tuple$i[${types(i)}]]
-    = Arbitrary(${gens}.map(Tuple1(_)))
-"""
+        |  /** Arbitrary instance of ${i}-Tuple */
+        |  implicit def arbTuple$i[${types(i)}](implicit
+        |    ${wrappedArgs("Arbitrary",i)}
+        |  ): Arbitrary[Tuple$i[${types(i)}]]
+        |    = Arbitrary(${gens}.map(Tuple1(_)))
+        |""".stripMargin
   } else {
     s"""
-  /** Arbitrary instance of ${i}-Tuple */
-  implicit def arbTuple$i[${types(i)}](implicit
-    ${wrappedArgs("Arbitrary",i)}
-  ): Arbitrary[Tuple$i[${types(i)}]]
-    = Arbitrary(Gen.zip(${gens}))
-"""
+        |  /** Arbitrary instance of ${i}-Tuple */
+        |  implicit def arbTuple$i[${types(i)}](implicit
+        |    ${wrappedArgs("Arbitrary",i)}
+        |  ): Arbitrary[Tuple$i[${types(i)}]]
+        |    = Arbitrary(Gen.zip(${gens}))
+        |""".stripMargin
   }
 }
 
@@ -87,45 +87,45 @@ def zip(i: Int) = {
 
   def sieveCopy = idents("g",i) zip idents("t",i) map { case (g,t) => s"$g.sieveCopy($t)" } mkString " && "
   s"""
-  /** Combines the given generators into one generator that produces a
-   *  tuple of their generated values. */
-  def zip[${types(i)}](
-    ${wrappedArgs("Gen",i)}
-  ): Gen[(${types(i)})] = {
-    val g = $gens
-    g.suchThat {
-      case (${vals(i)}) =>
-        ${sieveCopy}
-    }
-  }
-"""
+      |  /** Combines the given generators into one generator that produces a
+      |   *  tuple of their generated values. */
+      |  def zip[${types(i)}](
+      |    ${wrappedArgs("Gen",i)}
+      |  ): Gen[(${types(i)})] = {
+      |    val g = $gens
+      |    g.suchThat {
+      |      case (${vals(i)}) =>
+      |        ${sieveCopy}
+      |    }
+      |  }
+      |""".stripMargin
 }
 
 def resultOf(i: Int) = {
   def delegate = idents("T",i).drop(1).map("_:"+_).mkString(",")
   s"""
-  /** Takes a function and returns a generator that generates arbitrary
-   *  results of that function by feeding it with arbitrarily generated input
-   *  parameters. */
-  def resultOf[${types(i)},R]
-    (f: (${types(i)}) => R)
-    (implicit
-      ${wrappedArgs("Arbitrary",i)}
-    ): Gen[R] = arbitrary[T1] flatMap {
-      t => resultOf(f(t,$delegate))
-    }
-"""
+      |  /** Takes a function and returns a generator that generates arbitrary
+      |   *  results of that function by feeding it with arbitrarily generated input
+      |   *  parameters. */
+      |  def resultOf[${types(i)},R]
+      |    (f: (${types(i)}) => R)
+      |    (implicit
+      |      ${wrappedArgs("Arbitrary",i)}
+      |    ): Gen[R] = arbitrary[T1] flatMap {
+      |      t => resultOf(f(t,$delegate))
+      |    }
+      |""".stripMargin
 }
 
 def tupleCogen(i: Int) = {
   s"""
-  implicit final def tuple${i}[${types(i)}](implicit ${wrappedArgs("Cogen",i)}): Cogen[Tuple$i[${types(i)}]] =
-    Cogen((seed, t) =>
-      ${idents("c", i).zipWithIndex.foldLeft("seed"){
-        case (str, (c, n)) => s"$c.perturb($str, t._${n + 1})"
-      }}
-    )
-"""
+      |  implicit final def tuple${i}[${types(i)}](implicit ${wrappedArgs("Cogen",i)}): Cogen[Tuple$i[${types(i)}]] =
+      |    Cogen((seed, t) =>
+      |      ${idents("c", i).zipWithIndex.foldLeft("seed"){
+               case (str, (c, n)) => s"$c.perturb($str, t._${n + 1})"
+             }}
+      |    )
+      |""".stripMargin
 }
 
 /*
@@ -147,70 +147,70 @@ def functionCogen(i: Int) = {
   val fn = s"Function$i[${types(i)}, Z]"
 
   s"""
-  implicit final def function${i}[${types(i)}, Z](implicit ${wrappedArgs("Arbitrary",i)}, z: Cogen[Z]): Cogen[$fn] =
-    Cogen { (seed: Seed, f: $fn) =>
-${(1 to i).map(stanza).mkString}
-      Cogen[Z].perturb(seed, $ap)
-    }
-"""
+      |  implicit final def function${i}[${types(i)}, Z](implicit ${wrappedArgs("Arbitrary",i)}, z: Cogen[Z]): Cogen[$fn] =
+      |    Cogen { (seed: Seed, f: $fn) =>
+      |${(1 to i).map(stanza).mkString}
+      |      Cogen[Z].perturb(seed, $ap)
+      |    }
+      |""".stripMargin
 }
 
 val genAll: Seq[GeneratedFile] = Seq(
   GeneratedFile(
     "ArbitraryArities.scala",
     s"""/**
-Defines implicit [[org.scalacheck.Arbitrary]] instances for tuples and functions
-
-Auto-generated using project/codegen.scala
-*/
-package org.scalacheck
-
-private[scalacheck] trait ArbitraryArities{
-  // Functions //
-  ${1 to 22 map arbfn mkString ""}
-
-  // Tuples //
-  ${1 to 22 map tuple mkString ""}
-}"""),
+        |Defines implicit [[org.scalacheck.Arbitrary]] instances for tuples and functions
+        |
+        |Auto-generated using project/codegen.scala
+        |*/
+        |package org.scalacheck
+        |
+        |private[scalacheck] trait ArbitraryArities{
+        |  // Functions //
+        |  ${1 to 22 map arbfn mkString ""}
+        |
+        |  // Tuples //
+        |  ${1 to 22 map tuple mkString ""}
+        |}""".stripMargin),
   GeneratedFile(
     "GenArities.scala",
     s"""/**
-Defines zip and resultOf for all arities
-
-Auto-generated using project/codegen.scala
-*/
-package org.scalacheck
-private[scalacheck] trait GenArities{
-
-  // genfn //
-${1 to 22 map genfn mkString ""}
-
-  // zip //
-${1 to 22 map zip mkString ""}
-
-  // resultOf //
-  import Arbitrary.arbitrary
-  def resultOf[T,R](f: T => R)(implicit a: Arbitrary[T]): Gen[R]
-${2 to 22 map resultOf mkString ""}
-}"""
+        |Defines zip and resultOf for all arities
+        |
+        |Auto-generated using project/codegen.scala
+        |*/
+        |package org.scalacheck
+        |private[scalacheck] trait GenArities{
+        |
+        |  // genfn //
+        |${1 to 22 map genfn mkString ""}
+        |
+        |  // zip //
+        |${1 to 22 map zip mkString ""}
+        |
+        |  // resultOf //
+        |  import Arbitrary.arbitrary
+        |  def resultOf[T,R](f: T => R)(implicit a: Arbitrary[T]): Gen[R]
+        |${2 to 22 map resultOf mkString ""}
+        |}""".stripMargin
   ),
   GeneratedFile(
     "CogenArities.scala",
     s"""/**
-Auto-generated using project/codegen.scala
-*/
-package org.scalacheck
-private[scalacheck] abstract class CogenArities{
-
-  ${1 to 22 map tupleCogen mkString ""}
-
-  import Arbitrary.arbitrary
-  import Gen.Parameters.{ default => params }
-  import rng.Seed
-
-  ${1 to 22 map functionCogen mkString ""}
-
-}"""
+        |Auto-generated using project/codegen.scala
+        |*/
+        |package org.scalacheck
+        |private[scalacheck] abstract class CogenArities{
+        |
+        |  ${1 to 22 map tupleCogen mkString ""}
+        |
+        |  import Arbitrary.arbitrary
+        |  import Gen.Parameters.{ default => params }
+        |  import rng.Seed
+        |
+        |  ${1 to 22 map functionCogen mkString ""}
+        |
+        |}""".stripMargin
   )
 )
 }
